8572753b6744f1e457a9433a5f2f4f51535f291b
anayi89 <it.garry@gmail.com>
2018-07-29 22:34:53 -0400

    Create installRolesAndFeatures.ps1

---
+++ sysadmin/installRolesAndFeatures.ps1
@@ -0,0 +1,12 @@
+function RolesAndFeatures {
+  $rolesandfeatures = @("Bitlocker", "DHCP", "DNS", "GPMC", "Print-Services", "Remote-Desktop-Services", "SMTP-Server", "UpdateServices", "Web-MGMT-Service", "Web-Server", "Wireless-Networking")
+
+  foreach ($roleandfeature in $rolesandfeatures){
+  Install-WindowsFeature -Name $roleandfeature -IncludeManagementTools
+  Get-WindowsFeature -Name $roleandfeature
+  }
+  $pcname = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
+  Restart-Computer -ComputerName $pcname -Force
+}
+
+RolesAndFeatures
